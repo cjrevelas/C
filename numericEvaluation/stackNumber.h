f7a31cfabe4d650a1416d54f23790766ff6106c0
cjrevelas-teracloud <constantinos.revelas@teracloud.com>
2024-07-31 16:52:12 +0300

    style: this is a test commit trying some formatting changes

--- numericEvaluation/stackNumber.h
+++ numericEvaluation/stackNumber.h
@@ -1,7 +1,7 @@
 #ifndef STACK_NUMBER_H
 #define STACK_NUMBER_H
 
-void pushNumber(int* stackOfNumbers, int number, int* topNumberIndex, int numbers) {
+void pushNumber(int *stackOfNumbers, int number, int *topNumberIndex, int numbers) {
   if ((*topNumberIndex) == numbers-1) {
     printf("ERROR: stack overflow! Need to POP a value to continue properly!\n");
   } else {
@@ -10,7 +10,7 @@ void pushNumber(int* stackOfNumbers, int number, int* topNumberIndex, int number
   }
 }
 
-void popNumber(int* stackOfNumbers, int* topNumberIndex) {
+void popNumber(int *stackOfNumbers, int *topNumberIndex) {
   if ((*topNumberIndex) == -1) {
     printf("ERROR: stack underflow! Need to push a value to continue properly!\n");
   } else {
@@ -18,7 +18,7 @@ void popNumber(int* stackOfNumbers, int* topNumberIndex) {
   }
 }
 
-void displayStackOfNumbers(int* stackOfNumbers, int topNumberIndex) {
+void displayStackOfNumbers(int *stackOfNumbers, int topNumberIndex) {
   for (int jj=-1; jj<topNumberIndex; ++jj) {
     printf("%d ", stackOfNumbers[jj+1]);
   }
--- numericEvaluation/stackOperator.h
+++ numericEvaluation/stackOperator.h
@@ -1,7 +1,7 @@
 #ifndef STACK_OPERATOR_H
 #define STACK_OPERATOR_H
 
-void pushOperator(char* stackOfOperators, char ch, int* topOperatorIndex, int operators) {
+void pushOperator(char *stackOfOperators, char ch, int *topOperatorIndex, int operators) {
   if ((*topOperatorIndex) == operators-1) {
     printf("ERROR: stack overflow! Need to POP a value to continue properly!\n");
   } else {
@@ -11,7 +11,7 @@ void pushOperator(char* stackOfOperators, char ch, int* topOperatorIndex, int op
 }
 
 
-void popOperator(char* stackOfOperators, int* topOperatorIndex) {
+void popOperator(char *stackOfOperators, int *topOperatorIndex) {
   if ((*topOperatorIndex) == -1) {
     printf("ERROR: stack underflow! Need to push a value to continue properly!\n");
   } else {
@@ -20,7 +20,7 @@ void popOperator(char* stackOfOperators, int* topOperatorIndex) {
 }
 
 
-void displayStackOfOperators(char* stackOfOperators, int topOperatorIndex){
+void displayStackOfOperators(char *stackOfOperators, int topOperatorIndex){
   for (int ii=-1; ii<topOperatorIndex; ++ii) {
     printf("%c ", stackOfOperators[ii+1]);
   }
