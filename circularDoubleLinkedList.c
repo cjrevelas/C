9943cee1232934cb2f359536431d953865546ca1
cjrevelas <cjrevelas@gmail.com>
2025-09-25 23:34:48 +0300

    make the printing of the list better

--- circularDoubleLinkedList.c
+++ circularDoubleLinkedList.c
@@ -19,7 +19,7 @@ void createFirstNode( struct Node **head, struct Node ** tail, int value );
 void pushNodeFront( struct Node **head, struct Node **tail, int value );
 void pushNodeBack( struct Node **head, struct Node **tail, int value );
 void insertAfterNode( struct Node **head, struct Node **tail, int data, int newData );
-void printList( struct Node *head );
+void printList( struct Node *head, struct Node *tail );
 void printListReversed( struct Node *tail );
 
 
@@ -30,48 +30,36 @@ int main()
   struct Node *head = (struct Node *)malloc( sizeof( struct Node ) );
   struct Node *tail = (struct Node *)malloc( sizeof( struct Node ) );
 
-  printf( "\n&Head: %x, &Tail %x", &head, &tail );
+  printf( "\n&Head: %x, &Tail %x\n", &head, &tail );
+  printf( " Head: %x,  Tail %x\n",  head,  tail );
 
+  printf("\n\n");
+
+  printf("--> Creating the first node with value 10\n");
   createFirstNode( &head, &tail, 10 );
-  printList( head );
+  printList( head, tail );
 
-  printf( "--------------------------------------------------------------------\n" );
+  printf("--> Pushing front the value 20\n");
   pushNodeFront( &head, &tail, 20 );
-  printList( head );
-  printf( "\nHead: %x, Tail: %x\n", head, tail );
+  printList( head, tail );
 
-  printf( "--------------------------------------------------------------------\n" );
+  printf("--> Pushing back the value 30\n");
   pushNodeBack( &head, &tail, 30 );
-  printList( head );
-  printf( "\nHead: %x, Tail: %x\n", head, tail );
+  printList( head, tail );
 
-  printf( "--------------------------------------------------------------------\n" );
+  printf("--> Pushing front the value 50\n");
   pushNodeFront( &head, &tail, 50 );
-  printList( head );
-  printf( "\nHead: %x, Tail: %x\n", head, tail );
+  printList( head, tail );
 
-  printf( "--------------------------------------------------------------------\n" );
+  printf("--> Pushing back the value 80\n");
   pushNodeBack (&head, &tail, 80 );
-  printList( head );
-  printf( "\nHead: %x, Tail: %x\n", head, tail );
-  printf( "--------------------------------------------------------------------\n" );
+  printList( head, tail );
 
-  printf( "head->prev: %x, head->next: %x\n", head->prev, head->next );
-  printf( "tail->prev: %x, tail->next: %x\n", tail->prev, tail->next );
-  printf( "--------------------------------------------------------------------\n" );
-
-  printListReversed( tail );
-
-  printf( "--------------------------------------------------------------------\n" );
+  printf("--> Inserting value 120 after value 80\n");
   insertAfterNode (&head, &tail, 80, 120 );
-  printList( head );
-  printf( "\nHead: %x, Tail: %x\n", head, tail );
-  printf( "--------------------------------------------------------------------\n" );
+  printList( head, tail );
 
-  printf( "head->prev: %x, head->next: %x\n", head->prev, head->next );
-  printf( "tail->prev: %x, tail->next: %x\n", tail->prev, tail->next );
-  printf( "--------------------------------------------------------------------\n" );
-
+  printf("--> Printing list values in reversed order\n");
   printListReversed( tail );
   return 0;
 }
@@ -89,13 +77,6 @@ void createFirstNode( struct Node **head, struct Node **tail, int value )
   // Since only one node exists at this point, head and tail are the same
   //
   *tail = *head;
-
-  printf( "\n--------------------------------------------------------------------\n" );
-  printf( "List depth = %u\n", depth );
-  printf( "\nNode id: %x -> { %d, %x, %x }\n", *head, (*head)->data, (*head)->next, (*head)->prev );
-  printf( "\nHead: %x, Tail: %x\n", *head, *tail );
-  printf( "&Head: %x, &Tail %x\n", head, tail );
-  printf( "\nList status:\n" );
 
   return;
 }
@@ -179,16 +160,24 @@ void insertAfterNode( struct Node **head, struct Node **tail, int data, int newD
 }
 
 
-void printList( struct Node *head )
+void printList( struct Node *head, struct Node *tail )
 {
   struct Node *auxNode = head;
 
+  printf( "--------------------------------------------------------------------\n" );
+
   do
   {
-    printf( "%x %d\n", auxNode, auxNode->data );
+    printf( "%x:   { %d, %x, %x }\n", auxNode, auxNode->data, auxNode->prev, auxNode->next );
 
     auxNode = auxNode->next;
   } while ( auxNode != head );
+
+  printf( "\n" );
+  printf( "head = %x:   { %d, %x, %x }\n", head, head->data, head->prev, head->next );
+  printf( "tail = %x:   { %d, %x, %x }\n", tail, tail->data, tail->prev, tail->next );
+  printf( "--------------------------------------------------------------------\n" );
+  printf( "\n\n" );
 
   return;
 }
@@ -197,12 +186,17 @@ void printListReversed( struct Node *tail )
 {
   struct Node *auxNode = tail;
 
+  printf( "--------------------------------------------------------------------\n" );
+
   do
   {
     printf( "%x %d\n", auxNode, auxNode->data );
 
     auxNode = auxNode->prev;
   } while ( auxNode != tail );
+
+  printf( "--------------------------------------------------------------------\n" );
+  printf("\n");
 
   return;
 }
