3abb4aedd02ec5fae22747b6951bd7a62f495370
cjrevelas <cjrevelas@gmail.com>
2025-10-27 23:59:13 +0200

    add some comments

--- multithreading/fork_proc.c
+++ multithreading/fork_proc.c
@@ -1,6 +1,19 @@
 #include <unistd.h>
 #include <stdio.h>
 
+
+// The fork function is defined in unistd.h with the following prototype:
+//      pid_t fork(void);
+//
+// The getpid function is defined in unistd.h with the following prototype:
+//      pid_t getpid(void);
+//
+// where, pid_t, is a posix data type associated with an integer variable
+// and it is defined in the header sys/types.h
+//
+// We can also get the id of the parent of a certain process using the function
+// getppid() which is also defined in unistd.h
+
 int main()
 {
     fork();
